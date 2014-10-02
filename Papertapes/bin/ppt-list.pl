6e75d0af317151e9508da06203485aab1bae0f35
Computermuseum Arnhem <museum@hack42.nl>
2014-10-02 22:29:19 +0200

    papertapes, papertapes, more papertapes...

--- Papertapes/bin/ppt-list.pl
+++ Papertapes/bin/ppt-list.pl
@@ -11,9 +11,16 @@ foreach ( sort @ppt ) {
 
 	my $ini = Config::Tiny->read( $_ );
 	my $l = $ini->{ label };
+	my $b = $ini->{ location };
+	my $loc = '';
 
-	printf "%-25s %-25s %s\n", 
+	if ( $b->{ box } ) {
+		$loc = sprintf( "%02d/%02d", $b->{ box }, $b->{ slot } );
+	}	
+
+	printf "%-25s %-5s %-25s %s\n", 
 		dirname( $_ ) . ":",
+		$loc,		
 		$l->{ label }, 
 		join( " ", grep { $_ } ( $l->{ desc }, $l->{ desc1 }, $l->{ desc2 } ) );
 
